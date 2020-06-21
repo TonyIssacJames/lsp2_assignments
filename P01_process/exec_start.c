7cb0ce6de14d153e95f94cd1f9304c6d89a1636b
Tony Issac James <tonyissacjames@gmail.com>
2020-06-21 20:28:48 +0530

    forgot to add few CHANGES

--- P01_process/exec_start.c
+++ P01_process/exec_start.c
@@ -8,9 +8,9 @@
 
 int main()
 {
-	//char *argv_list[] = {NULL};
-	char *argv_list[] = {"-l", NULL};
-	char *const envp[] = {"/bin/", NULL};
+	char *argv_list[] = {NULL};
+	//char *argv_list[] = {"-l", NULL};
+	//char *const envp[] = {"/bin/", NULL};
 
 	printf("Before exec my id is %d\n", getpid());
 	printf("My Parent process's id is %d\n", getppid());
@@ -18,8 +18,8 @@ int main()
 	printf("exec starts\n");
         // TODO 1: Call exec with exec_overlay.exe as an argument
 
-	//execv("exec_overlay.exe", argv_list);
-	execvpe("ls", argv_list, envp);
+	execv("exec_overlay.exe", argv_list);
+	//execvpe("ls", argv_list, envp);
 	printf("This won't print\n");
 	return 0;
 }
